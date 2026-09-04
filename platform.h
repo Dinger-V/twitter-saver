431ca6a6c7e1bf55da23a7ce02be67fae2ab32b4
Dinger <dinger.xyz@proton.me>
2026-09-04 23:05:51 +0700

    Refactor platform.h includes and define S_ISDIR

--- platform.h
+++ platform.h
@@ -1,14 +1,19 @@
 #ifndef PLATFORM_H
 #define PLATFORM_H
 
+#include <stdlib.h>
+#include <sys/stat.h>
+
 #ifdef _WIN32
   #include <direct.h>
-  #include <stdlib.h>
   #define get_home() getenv("USERPROFILE")
   #define make_dir(path, mode) _mkdir(path)
+
+  
+  #ifndef S_ISDIR
+    #define S_ISDIR(mode) (((mode) & S_IFMT) == S_IFDIR)
+  #endif
 #else
-  #include <sys/stat.h>
-  #include <stdlib.h>
   #define get_home() getenv("HOME")
   #define make_dir(path, mode) mkdir(path, mode)
 #endif
