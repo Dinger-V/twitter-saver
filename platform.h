4d1a8d9ca500d78e040d04d6cbd8be790c613967
Dinger <dinger.xyz@proton.me>
2026-09-05 18:29:20 +0700

    Update platform.h
    fixed weird indentation and add uninst to prevent compiler warning

--- platform.h
+++ platform.h
@@ -5,17 +5,17 @@
 #include <sys/stat.h>
 
 #ifdef _WIN32
-  #include <direct.h>
-  #define get_home() getenv("USERPROFILE")
-  #define make_dir(path, mode) _mkdir(path)
+    #include <direct.h>
+    #define get_home() getenv("USERPROFILE")
+    #define make_dir(path, mode) _mkdir(path)
 
-  
-  #ifndef S_ISDIR
-    #define S_ISDIR(mode) (((mode) & S_IFMT) == S_IFDIR)
-  #endif
+    #ifndef S_ISDIR
+        #define S_ISDIR(mode) (((mode) & S_IFMT) == S_IFDIR)
+    #endif
 #else
-  #define get_home() getenv("HOME")
-  #define make_dir(path, mode) mkdir(path, mode)
+    #include <unistd.h>
+    #define get_home() getenv("HOME")
+    #define make_dir(path, mode) mkdir(path, mode)
 #endif
 
 #endif
